f77e49ed827a6b3ef4fcf88721ddafd8483964c0
Huluette <leana.beuvant@gmail.com>
2023-11-28 16:51:44 +0100

    Création d'un quizz sans l'appellation d'un CSV

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace QuizApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Création des questions
+            string[] questions =
+            {
+                "Quel est l'animal le plus rapide au monde ?",
+                "Qui bas le record du monde de jeu de fléchettes ?",
+                "En quelle année est décédée Elizabeth II ?"
+            };
+
+            string[] answers =
+            {
+                "A. HamsterZila \nB. BrebisPhaon \nC. SanglierBinouz",
+                "A. Jean-Jacques Coldman \nB. Mylène Fermière \nC. Renault la voiture \nD. Dieu pas donné",
+                "A. 2022 \nB. 2023 \nC. 2024"
+            };
+
+            int[] correctAnswers =
+            {
+                1, 3, 0 
+            };
+            int playerScore = 0;
+
+
+            Console.WriteLine("Bienvenue dans la meilleure application de Quizz claquée au sol ! :)");
+            for (int iplayerScore= 0;  iplayerScore<questions.Length; iplayerScore++)
+            {
+                Console.WriteLine("Questions " + (iplayerScore + 1));
+                Console.WriteLine(questions[iplayerScore]);
+                Console.WriteLine(answers[iplayerScore]);
+                Console.WriteLine("S'il vous plait, entrez votre réponse ('A', 'B', 'C' ou 'D'): ");
+                string playerAnswer = Console.ReadLine();
+
+                // Valider les réponses
+
+                if(string.Equals(playerAnswer, "A", StringComparison.OrdinalIgnoreCase) && correctAnswers[iplayerScore] == 0)
+                {
+                    playerScore++;
+                }
+                else if(string.Equals(playerAnswer, "B", StringComparison.OrdinalIgnoreCase) && correctAnswers[iplayerScore] == 1)
+                {
+                    playerScore++;
+                }
+                else if(string.Equals(playerAnswer, "C", StringComparison.OrdinalIgnoreCase) && correctAnswers[iplayerScore] == 2)
+                {
+                    playerScore++;
+                }
+                else if(string.Equals(playerAnswer, "D", StringComparison.OrdinalIgnoreCase) && correctAnswers[iplayerScore] == 3)
+                {
+                    playerScore++;
+                }
+            }
+
+            // Afficher le score de l'utilisateur
+            Console.WriteLine("Le Quizz est terminé !");
+            Console.WriteLine("Bravo ! Ton score est de : " + playerScore + "/" + questions.Length);
+
+        }
+    }
+}
---
+++ ConsoleApp1/Readme.md
@@ -0,0 +1,60 @@
+# Projet QuizzConsole
+
+## > Récupérer le projet
+
+Bienvenue, pour récupérer le projet il suffit de... (explications à venir)
+
+## > Lancer le projet
+
+Pour lancer le projet...
+
+## > Comprendre le projet
+
+Pour comprendre le projet il suffit de suivre les étapes du jeu de Quizz.
+
+Pour bien débuter, on va tout d'abord commencer avec une seule catégorie au début. Chaque titre et sous titre de ce readme.md doit être transfomé en méthode. 
+
+## > Ecrire les questions en 'dur' dans le code 
+
+3 questions récupérées en 'dur' dans un premier temps :
+
+- ```var questionUne = "Hey ! Salut, ca va l'ami.e ?"```
+- ```var ReponseUne = "Je pète le feu et toi ?!"```
+- ```var ReponseDeux = "!j'ai connu des jours meilleurs..."```
+- ```var ReponseTrois = "Je suis grave dans la m*** !"```
+
+## Depuis un fichier dans un second temps
+	
+On peut utiliser le fichier.... et l'importer de ka manière suivante : 
+
+```Blabla code, blabla code```
+
+### Récupérer la première ligne du fichier CSV
+
+Il faut utiliser la méthoe ... pour pouvoir récupérer "Séparer les champs"...
+
+### Récupérer la liste des questions
+
+Quelle est la structure de ma liste de questions ? 
+> **Les différents type de collection**
+> 1. List <>
+> 2. Dictionnary<>
+> 3. string[]
+
+Si on fait un tableau de string pour lister les intitulés, comment lier chaque question à ses réponses ?
+Il est temps de construire des classes : une classe Question qui contient une liste de réponse.
+
+## Accueillir le joueur 
+
+## Demander la catégorie (dans un second temps) et filtrer la liste des questions 
+
+## Parcourir les questions (boucle)
+
+>Pour chacune d'entre elles 
+>1. **Poser la question**
+>2. **Donner les réponses possibles**
+>3. **Vérifier si la réponse est bonne/mauvaise/une erreur**
+>4. **Informer l'utilisateur du résultat et afficher son score**
+>5. **Boucler**
+
+## Afficher un message d'au revoir avec le score lorsque l'ensemble des questions ont été posées 
